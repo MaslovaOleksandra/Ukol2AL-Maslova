7e8be79203fbaee8c6a981904ecac3c1bfae5b5c
MaslovaOleksandra <159123855+MaslovaOleksandra@users.noreply.github.com>
2024-11-20 08:41:21 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,28 @@
+import random
 
+arraya = []
+for _ in range(10):
+    pole.append(random.randint(0, 100))
+
+print(arraya)
+
+def bubble_sort(array):
+  n = len(arr)
+
+  for i in range(n):
+ 
+ in place
+    for j in range(0, n-i-1):
+
+      if arr[j] > arr[j+1] :
+        arr[j], arr[j+1] = arr[j+1], arr[j]   
+
+
+arr = [64, 34, 25, 12, 22, 11, 90]
+
+bubble_sort(arr)
+
+print("Sorted   
+ array:")
+for i in range(len(arr)):
+  print("%d" % arr[i])
